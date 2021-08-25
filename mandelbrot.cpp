7451a62509f81325387ae5d17778db30d37f3d1b
qawbecrdtey <qawbecrdtey@kaist.ac.kr>
2021-08-25 17:41:52 +0900

    Modified . Now, the iteration multiplier increases exponentially. CPU time and current color status is printed.

--- mandelbrot.cpp
+++ mandelbrot.cpp
@@ -1,222 +1,211 @@
-#include <iRRAM.h>
-#include "iRRAMx/polynomial.hpp"
+#include <iRRAM/lib.h>
+#include <iRRAMx/polynomial.hpp>
 
-#include <fstream>
+#include <memory>
 #include <set>
+#include <ctime>
 
-using namespace iRRAM;
-
-constexpr int multiplier = 1024;
-
-class painter {
-    int real_resolution, imag_resolution;
-    REAL real_lo, real_hi;
-    REAL imag_lo, imag_hi;
-    REAL distance2;
-    bool computed;
-
-    std::set<std::pair<int, int>> unknown;
-    std::set<std::pair<std::pair<int, int>, INTEGER>> exterior;
-    std::set<std::pair<std::pair<int, int>, INTEGER>> interior;
-    std::set<std::pair<int, int>> boundary;
-
-    COMPLEX int_pair_to_COMPLEX(std::pair<int, int> const &p) const {
-        return { real_lo + (real_hi - real_lo) / (real_resolution << 1) + (real_hi - real_lo) / real_resolution * p.first,
-                 imag_lo + (imag_hi - imag_lo) / (imag_resolution << 1) + (imag_hi - imag_lo) / imag_resolution * p.second };
+void print_current(int resolution, std::unique_ptr<std::unique_ptr<std::unique_ptr<iRRAM::INTEGER>[]>[]> const &canvas) {
+    for(int j = 0; j < resolution; j++) {
+        for(int i = 0; i < resolution; i++) {
+            if(!canvas[i][j]) { printf("?"); continue; }
+            if(*canvas[i][j] < 0) { printf(" "); continue; }
+            if(*canvas[i][j] > 0) { printf("#"); continue; }
+            printf("~");
+        } printf("\n");
     }
+}
 
-    COMPLEX INTEGER_pair_to_COMPLEX(std::pair<INTEGER, INTEGER> const &p, INTEGER const &resolution) const {
-        return { real_lo + (real_hi - real_lo) / (resolution << 1) + (real_hi - real_lo) / resolution * p.first,
-                 imag_lo + (imag_hi - imag_lo) / (resolution << 1) + (imag_hi - imag_lo) / resolution * p.second };
-    }
+iRRAM::COMPLEX int_pair_to_COMPLEX(int resolution, std::pair<int, int> const &p) {
+    return {
+        -2 + iRRAM::REAL(2) / resolution + iRRAM::REAL(4) * p.first / resolution,
+        -2 + iRRAM::REAL(2) / resolution + iRRAM::REAL(4) * p.second / resolution
+    };
+}
+iRRAM::COMPLEX INTEGER_pair_to_COMPLEX(int resolution, std::pair<INTEGER, INTEGER> const &p) {
+    return {
+            -2 + iRRAM::REAL(2) / resolution + iRRAM::REAL(4) * p.first / resolution,
+            -2 + iRRAM::REAL(2) / resolution + iRRAM::REAL(4) * p.second / resolution
+    };
+}
 
-public:
-    painter(int real_resolution, int imag_resolution, REAL real_lo, REAL real_hi, REAL imag_lo, REAL imag_hi)
-    : real_resolution(real_resolution), imag_resolution(imag_resolution),
-      real_lo(std::move(real_lo)), real_hi(std::move(real_hi)),
-      imag_lo(std::move(imag_lo)), imag_hi(std::move(imag_hi)),
-      distance2(maximum((real_hi - real_lo) / real_resolution, (imag_hi - imag_lo) / imag_resolution)),
-      computed(false) {
-        distance2 = distance2 * distance2;
-        for(int i = 0; i < real_resolution; i++) {
-            for(int j = 0; j < imag_resolution; j++) {
-                unknown.insert({ i, j });
-            }
+uint64_t reiteration_count;
+
+void compute(int const &resolution, std::unique_ptr<std::unique_ptr<std::unique_ptr<iRRAM::INTEGER>[]>[]> const &canvas) {
+    for(int i = 0; i < resolution; i++) {
+        for(int j = 0; j < resolution; j++) {
+            canvas[i][j] = nullptr;
         }
     }
+    bool loop = true;
+    iRRAM::REAL const distance(REAL(4) / resolution);
+    iRRAM::REAL const distance2(distance * distance);
+    iRRAM::INTEGER iteration_power2(2);
+    iRRAM::INTEGER iteration_exterior(1), iteration_exterior_next(2048), iteration_interior(1), iteration_interior_next(2);
+    std::unique_ptr<std::unique_ptr<std::unique_ptr<iRRAM::COMPLEX>[]>[]> storage_exterior;
+    std::unique_ptr<std::unique_ptr<std::unique_ptr<iRRAM::POLYNOMIAL>[]>[]> storage_interior;
 
-    void compute() {
-        INTEGER iteration = 0;
-        INTEGER iteration_power2 = 1;
-        while(!unknown.empty()) {
-            cout << "iteration #" << iteration << "\n";
+    /// INITIALIZATION
+    storage_exterior = std::make_unique<std::unique_ptr<std::unique_ptr<iRRAM::COMPLEX>[]>[]>(resolution);
+    storage_interior = std::make_unique<std::unique_ptr<std::unique_ptr<iRRAM::POLYNOMIAL>[]>[]>(resolution);
+    for(int i = 0; i < resolution; i++) {
+        storage_exterior[i] = std::make_unique<std::unique_ptr<iRRAM::COMPLEX>[]>(resolution);
+        storage_interior[i] = std::make_unique<std::unique_ptr<iRRAM::POLYNOMIAL>[]>(resolution);
+    }
 
-            // EXTERIOR
-            // std::cout << "EXTERIOR\n";
-            for(auto it = unknown.begin(); it != unknown.end();) {
-                auto const c = int_pair_to_COMPLEX(*it);
-                auto x = c;
+printf("BEGIN: reiteration = %lu\n", reiteration_count++);
+    clock_t start, end;
+
+    while(loop) {
+        loop = false;
+        for(int i = 0; i < resolution; i++) {
+            for(int j = 0; j < resolution; j++) {
+                if(canvas[i][j]) continue;
+printf("working on (%d, %d)...\n", i, j);
+                loop = true;
+                auto const c = int_pair_to_COMPLEX(resolution, { i, j });
                 bool flag = false;
-                for(INTEGER iter = 0; iter < iteration * multiplier; iter = iter + 1) {
-                    x = x * x + c;
-                }
-                for(int iter = 0; iter < multiplier; iter++) {
-                    auto const d = real(x) * real(x) + imag(x) * imag(x);
-                    if(choose(d > 4, d < 5) == 1) {
-                        exterior.insert({ std::move(*it), -(iteration * multiplier + iter + 1) });
-                        it = unknown.erase(it);
+
+printf("### EXTERIOR ###\n");
+                /// EXTERIOR
+                start = clock();
+                auto z = (storage_exterior[i][j] ? *storage_exterior[i][j] : COMPLEX(REAL(0)));
+                for(auto iter = iteration_exterior; iter < iteration_exterior_next; iter = iter + 1) {
+                    z = z * z + c;
+                    auto const d2 = real(z) * real(z) + imag(z) * imag(z);
+                    if(choose(d2 > 4, d2 < 5) == 1) {
+                        canvas[i][j] = std::make_unique<INTEGER>(-iter);
+                        storage_exterior[i][j] = nullptr;
                         flag = true;
                         break;
                     }
-                    x = x * x + c;
                 }
-                if(!flag) ++it;
-            }
+                end = clock();
+printf("Elapsed time: %Lf\n", ((long double)(end - start)) / CLOCKS_PER_SEC);
+                print_current(resolution, canvas);
+                if(flag) continue;
 
-            // INTERIOR
-            // std::cout << "INTERIOR\n";
-            for(auto it = unknown.begin(); it != unknown.end();) {
-                auto const c = int_pair_to_COMPLEX(*it);
-                POLYNOMIAL const p(2, { c, REAL(0), REAL(1) });
-                POLYNOMIAL P = p;
-                //std::cout << "Working on: " << it->first << ' ' << it->second << '\n';
-                //cout << x << "\n";
-                for(INTEGER i = 0; i < iteration; i = i + 1) {
-                    P = composite(P, p);
-                }
-
-                P.coef[1] = REAL(-1);
-                auto const v = roots(P);
-                P.coef[1] = REAL(0);
-                auto const D = deriv(P, 1);
-                bool flag = false;
-
-                if(std::any_of(v.cbegin(), v.cend(), [&D](const COMPLEX& c) {
-                    auto const val = abs(D(c));
-                    return choose(val < 1, val > 0.5) == 1;
-                })) {
-                    interior.insert({ std::move(*it), iteration + 1 });
-                    it = unknown.erase(it);
-                    flag = true;
-                    break;
-                }
-                if(!flag) ++it;
-            }
-
-            // HELPER GRID
-            // std::cout << "HELPER GRID\n";
-            std::set<std::pair<INTEGER, INTEGER>> grid_exterior;
-            std::set<std::pair<INTEGER, INTEGER>> grid_interior;
-            for(INTEGER i = 1; i < iteration_power2; i = i + 2) {
-                for(INTEGER j = 1; j < iteration_power2; j = j + 2) {
-                    auto const c = INTEGER_pair_to_COMPLEX({ i, j }, iteration_power2);
-                    auto x = c;
-                    bool flag = false;
-
-                    // EXTERIOR
-                    for(INTEGER i = 0; i < iteration * multiplier; i = i + 1) {
-                        auto const d = real(x) * real(x) + imag(x) * imag(x);
-                        if(choose(d > 4, d < 5) == 1) {
-                            grid_exterior.insert({ i, j });
+printf("### INTERIOR ###\n");
+                /// INTERIOR
+                start = clock();
+                auto const P = iRRAM::POLYNOMIAL(2, { c, REAL(0), REAL(1) });
+                auto p = (storage_interior[i][j] ? *storage_interior[i][j] : iRRAM::POLYNOMIAL(1, { REAL(0), REAL(1) }));
+                for(auto iter = iteration_interior; iter < iteration_interior_next; iter = iter + 1) {
+                    p = iRRAM::composite(p, P);
+                    p.coef[1] = REAL(-1);
+                    auto const rootv = iRRAM::roots(p);
+                    p.coef[1] = REAL(0);
+                    auto const D = iRRAM::deriv(p, 1);
+                    for(auto const &r : rootv) {
+                        auto const x = D(r);
+                        auto const d2 = real(x) * real(x) + imag(x) * imag(x);
+                        if(choose(d2 < 1, d2 > 0.5) == 1) {
+                            canvas[i][j] = std::make_unique<INTEGER>(iter);
+                            storage_exterior[i][j] = nullptr;
                             flag = true;
                             break;
                         }
-                        x = x * x + c;
                     }
-                    if(flag) break;
+                }
+                end = clock();
+printf("Elapsed time: %Lf\n", ((long double)(end - start)) / CLOCKS_PER_SEC);
+                print_current(resolution, canvas);
+                if(flag) continue;
 
-                    // INTERIOR
-                    POLYNOMIAL const p(2, { c, REAL(0), REAL(1) });
-                    POLYNOMIAL P = p;
-                    for(INTEGER i = 0; i < iteration; i = i + 1) {
-                        P.coef[1] = REAL(-1);
-                        auto const v = roots(P);
-                        P.coef[1] = REAL(0);
-                        auto const D = deriv(P, 1);
-
-                        if(std::any_of(v.cbegin(), v.cend(), [&D](const COMPLEX& c) {
-                            auto const val = abs(D(c));
-                            return choose(val < 1, val > 0.5) == 1;
-                        })) {
-                            grid_interior.insert({ i, j });
-                            break;
+printf("### BOUNDARY ###\n");
+                /// HELPER GRID & BOUNDARY
+                start = clock();
+                bool grid_exterior = false;
+                bool grid_interior = false;
+                for(iRRAM::INTEGER x = 1; x < iteration_power2; x = x + 2) {
+                    for(iRRAM::INTEGER y = 1; y < iteration_power2; y = y + 2) {
+                        auto const cur = INTEGER_pair_to_COMPLEX(resolution, { x, y });
+                        auto const d = cur - c;
+                        auto const d2 = real(d) * real(d) + imag(d) * imag(d);
+                        if(choose(d2 < distance2, d2 > distance2 / 2) == 1) {
+                            bool inner_flag = false;
+                            z = REAL(0);
+                            for(iRRAM::INTEGER iter = 1; iter < iteration_exterior_next; iter = iter + 1) {
+                                z = z * z + c;
+                                auto const d2 = real(z) * real(z) + imag(z) * imag(z);
+                                if(choose(d2 > 4, d2 < 5) == 1) {
+                                    grid_exterior = true;
+                                    inner_flag = true;
+                                    break;
+                                }
+                            }
+                            if(inner_flag) {
+                                if(grid_interior) {
+                                    canvas[i][j] = std::make_unique<iRRAM::INTEGER>(0);
+                                    storage_exterior[i][j] = nullptr;
+                                    storage_interior[i][j] = nullptr;
+                                    goto END;
+                                }
+                                else continue;
+                            }
+                            auto const P = iRRAM::POLYNOMIAL(2, { cur, REAL(0), REAL(1) });
+                            p = iRRAM::POLYNOMIAL(1, { REAL(0), REAL(1) });
+                            for(iRRAM::INTEGER iter = 1; iter < iteration_interior_next; iter = iter + 1) {
+                                p = composite(p, P);
+                                p.coef[1] = REAL(-1);
+                                auto const rootv = iRRAM::roots(p);
+                                p.coef[1] = REAL(0);
+                                auto const D = iRRAM::deriv(p, 1);
+                                for(auto const &r : rootv) {
+                                    auto const tmp = D(r);
+                                    auto const tmp2 = real(tmp) * real(tmp) + imag(tmp) * imag(tmp);
+                                    if(choose(tmp2 < 1, tmp2 > 0.5) == 1) {
+                                        grid_interior = true;
+                                        inner_flag = true;
+                                        break;
+                                    }
+                                }
+                            }
+                            if(inner_flag) {
+                                if(grid_exterior) {
+                                    canvas[i][j] = std::make_unique<iRRAM::INTEGER>(0);
+                                    storage_exterior[i][j] = nullptr;
+                                    storage_interior[i][j] = nullptr;
+                                    goto END;
+                                }
+                                else continue;
+                            }
                         }
-
-                        if(i != iteration - 1) P = composite(P, p);
                     }
                 }
+                end = clock();
+printf("Elapsed time: %Lf\n", ((long double)(end - start)) / CLOCKS_PER_SEC);
+                print_current(resolution, canvas);
+            END:;
             }
-
-            // BOUNDARY
-            // std::cout << "BOUNDARY\n";
-            for(auto it = unknown.begin(); it != unknown.end();) {
-                bool flag = false;
-                auto const point = int_pair_to_COMPLEX(*it);
-                for(auto extr = grid_exterior.begin(); extr != grid_exterior.end(); ++extr) {
-                    auto const ext_point = INTEGER_pair_to_COMPLEX(*extr, iteration_power2);
-                    auto const ext_diff = ext_point - point;
-                    auto const ext_dist2 = real(ext_diff) * real(ext_diff) + imag(ext_diff) * imag(ext_diff);
-                    if(choose(ext_dist2 < distance2, ext_dist2 > distance2 / 2) != 1) continue;
-
-                    for(auto intr = grid_interior.begin(); intr != grid_interior.end(); ++intr) {
-                        auto const int_point = INTEGER_pair_to_COMPLEX(*intr, iteration_power2);
-                        auto const int_diff = int_point - point;
-                        auto const int_dist2 = real(int_diff) * real(int_diff) + imag(int_diff) * imag(int_diff);
-                        if(choose(int_dist2 < distance2, int_dist2 > distance2 / 2) != 1) continue;
-
-                        boundary.insert(std::move(*it));
-                        it = unknown.erase(it);
-                        flag = true;
-                        break;
-                    }
-
-                    if(flag) break;
-                }
-                if(!flag) ++it;
-            }
-
-            iteration = iteration + 1;
-            iteration_power2 = (iteration_power2 << 1);
         }
-        computed = true;
+
+        iteration_exterior = iteration_exterior_next;
+        iteration_exterior_next = (iteration_exterior_next << 1);
+        iteration_interior = iteration_interior_next;
+        iteration_interior_next = (iteration_interior_next + 1);
+        iteration_power2 = (iteration_power2 << 1);
+    }
+}
+
+int main(int argc, char *argv[]) {
+    int resolution;
+    reiteration_count = 0;
+    iRRAM_initialize(argc, argv);
+
+    std::cout << "Input resolution: ";
+    std::cin >> resolution;
+
+    auto canvas = std::make_unique<std::unique_ptr<std::unique_ptr<iRRAM::INTEGER>[]>[]>(resolution);
+    for(int i = 0; i < resolution; i++) {
+        canvas[i] = std::make_unique<std::unique_ptr<iRRAM::INTEGER>[]>(resolution);
     }
 
-    void draw(std::ostream &os) {
-        if(!computed) compute();
-        auto canvas = new INTEGER*[imag_resolution];
-        for(int j = 0; j < imag_resolution; j++) {
-            canvas[j] = new INTEGER[real_resolution];
-            for(int i = 0; i < real_resolution; i++) {
-                canvas[j][i] = 0;
-            }
-        }
-        for(auto const &now : exterior) {
-            canvas[now.first.second][now.first.first] = now.second;
-        }
-        for(auto const &now : interior) {
-            canvas[now.first.second][now.first.first] = now.second;
-        }
-        for(auto const &now : boundary) {
-            canvas[now.second][now.first] = 0;
-        }
+    iRRAM::exec(compute, resolution, canvas);
 
-        for(int i = 0; i < imag_resolution; i++) {
-            for(int j = 0; j < real_resolution; j++) {
-                os << std::setfill(' ') << std::setw(8) << (int)canvas[i][j];
-            } os << '\n';
-        }
-
-        for(int i = 0; i < imag_resolution; i++) {
-            delete[] canvas[i];
-        } delete[] canvas;
-    }
-};
-
-constexpr int resolution = 17;
-
-void compute() {
-    painter p(resolution, resolution, -2, 2, -2, 2);
-    std::ofstream ofs("result.txt");
-    p.draw(ofs);
-    ofs.close();
+    for(int j = 0; j < resolution; j++) {
+		for(int i = 0; i < resolution; i++) {
+			std::cout << swrite(*canvas[i][j]) << ' ';
+		} std::cout << '\n';
+	}
 }
